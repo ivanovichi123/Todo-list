f1949548c298cfe5d9a5825e1a4c468dbc52fb62
ivanovichi <ivan.arthur18@gmail.com>
2025-02-21 14:47:32 -0600

    All the instals  are complete and the process for the initial load is beginning

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./style.css";
 console.log(greeting);
+
+
+//THE PLAN
+//CURRENT: First I put in order the header, sidebar, projectSpace, footer
+//NEXT: Make a js for the initial load of the sidebar and projectSpace
+//NEXT: Make a js for the constant content of the header and the footer
+//NEXT: Create the next plan for making a button that triggers a dialog which allows the submit of a new project
