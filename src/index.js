a0014a7c5cbd241bda9322761893630ccdc1192d
ivanovichi <ivan.arthur18@gmail.com>
2025-02-24 09:48:26 -0600

    Skeleton ready
    The header, sidebar, space for the projects and
    footer are all set in their place

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log(greeting);
 
 
 //THE PLAN
-//CURRENT: First I put in order the header, sidebar, projectSpace, footer
-//NEXT: Make a js for the initial load of the sidebar and projectSpace
+//COMPLETE: First I put in order the header, sidebar, projectSpace, footer
+//CURRENT: Make a js for the initial load of the sidebar and projectSpace
 //NEXT: Make a js for the constant content of the header and the footer
 //NEXT: Create the next plan for making a button that triggers a dialog which allows the submit of a new project
