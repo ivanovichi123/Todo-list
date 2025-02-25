69ccad0ce5a8f0d3e92c2bbc4f19df30c297c4eb
ivanovichi <ivan.arthur18@gmail.com>
2025-02-25 15:10:55 -0600

    Header is ready

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import "./style.css";
 import { initialSidebar } from "./loadSidebar";
 import { initialProject } from "./loadProject";
 import { initialFooter } from "./initialfooter";
+import { initialHeader } from "./intialheader";
 
 function deployment () {
     initialSidebar();
     initialProject();
     initialFooter();
+    initialHeader();
 }
 
 console.log("index js");
